a5d72cffc806f168886c914530797bc5593ee783
ahmad <ahmadd13831383@gmail.com>
2026-08-08 21:25:11 +0330

    add sign in

--- account/views.py
+++ account/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 
 
 
@@ -16,6 +17,29 @@ def user_login(request):
             login(request, user)
             return redirect('/')
     return render(request, 'account/login.html', )
+
+
+def user_register(request):
+    context = {'errors' : []}
+    if request.user.is_authenticated == True:
+        return redirect('/')
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password1 = request.POST.get('password1')
+        password2 = request.POST.get('password2')
+        if password1 != password2:
+            context['errors'].append('not similar passwords')
+            return render(request, 'account/register.html', context)
+
+        user = User.objects.create(username=username, password=password1, email=email)
+        login(request, user)
+        return redirect('/')
+
+        return render(request, 'account/register.html', context)
+        return redirect('/')
+    return render(request, 'account/register.html', {})
 
 
 def user_logout(request):
