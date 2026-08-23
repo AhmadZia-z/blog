ddee0f05e1904bfd8ccdb31274dbd52248cf9d65
ahmad <ahmadd13831383@gmail.com>
2026-08-23 10:52:41 +0330

    add sidebar

---
+++ context_processors/context_processors.py
@@ -0,0 +1,13 @@
+from blog.models import Article, Caregory
+
+
+
+
+def recent_articles(request):
+    recent_articles = Article.objects.order_by('-created')[:3]
+    return {'recent_articles' : recent_articles}
+
+
+def category(request):
+    category = Caregory.objects.all()
+    return {'category' : category}
--- home/views.py
+++ home/views.py
@@ -5,6 +5,4 @@ from django.urls import reverse
 
 def home(request):
     articles = Article.objects.all()
-    recent_articles = Article.objects.all().order_by('-created')[:3]
-    category = Caregory.objects.all()
-    return render(request, 'home/index.html',{'articles' : articles, 'recent_articles' : recent_articles, 'category' : category})
+    return render(request, 'home/index.html',{'articles' : articles})
